d93fad409ddbb836c9f021bda8305f8d1aeea9eb
Jack Rizza <jackrizza@gmail.com>
2016-02-17 14:36:32 -0500

    First commit

---
+++ main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        // Declared name
+        String name = "sebi is the best";
+        for(int i = 0; i < 100;i++) {
+            // had it print name
+            System.out.println( name );
+        }
+    }
+}
